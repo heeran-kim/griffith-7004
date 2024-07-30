56a903c6bed8ff4369aeda9d0e3647ccbfcca201
Heeran Kim <heerankim3@gmail.com>
2024-07-31 09:28:26 +1000

    AWT: Setting Up an AWT Application

---
+++ src/lab2/ex1/ex1/Main.java
@@ -0,0 +1,11 @@
+package lab2.ex1.ex1;
+
+import java.awt.Frame;
+
+public class Main {
+    public static void main(String[] args) {
+        Frame frame = new Frame("AWT Frame");
+        frame.setSize(400, 300);
+        frame.setVisible(true);
+    }
+}
